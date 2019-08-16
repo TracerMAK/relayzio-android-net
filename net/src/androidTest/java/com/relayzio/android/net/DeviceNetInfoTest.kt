9994bc5723daf721cf463fe3e3f4a8968fd038fc
Mark <tsmckiel@live.com>
2019-08-16 02:52:26 -0500

    Added methods for network connection and ip info in DeviceNetInfo

--- net/src/androidTest/java/com/relayzio/android/net/DeviceNetInfoTest.kt
+++ net/src/androidTest/java/com/relayzio/android/net/DeviceNetInfoTest.kt
@@ -1,11 +1,19 @@
+/**
+ * For log output use the following adb command:
+ *   adb logcat RelayzIO_android_net:D *:S
+ */
+
 package com.relayzio.net
 
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
+import com.relayzio.android.net.DeviceNetInfo
+import org.junit.Assert
 import org.junit.Assume
 import org.junit.Before
 import org.junit.Test
@@ -14,6 +22,8 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class DeviceNetInfoTest {
+    // Context should never be null so just use lazy initialization.
+    private lateinit var context: Context
     private var connectionType: Int? = null
 
     /**
@@ -24,23 +34,50 @@ class DeviceNetInfoTest {
     @Before
     fun getConnectionType() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        context = InstrumentationRegistry.getInstrumentation().targetContext
         // Get the service for network connections and check for active network.
-        val cm = appContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
         connectionType = activeNetwork?.type
     }
 
+    /**
+     * Test that a mobile network can be found on the device as long as it is
+     * enabled and wifi is off. If the mobile network is not enabled or wifi
+     * is on, the test will be ignored.
+     */
     @Test
     fun mobileConnectionPresent() {
-        //val info = DeviceNetInfo()
-        //Assert.assertNotNull(info)
-
+        val info = DeviceNetInfo(context)
+        Assert.assertNotNull(info.connectionType())
         Assume.assumeTrue(connectionType == ConnectivityManager.TYPE_MOBILE)
+
+        Assert.assertEquals(connectionType, info.connectionType())
     }
 
+    /**
+     * Test that a wifi network can be found if enabled. If wifi is turned
+     * off on the device, the test will be ignored.
+     */
     @Test
     fun wifiConnectionPresent() {
+        val info = DeviceNetInfo(context)
+        Assert.assertNotNull(info.connectionType())
         Assume.assumeTrue(connectionType == ConnectivityManager.TYPE_WIFI)
+
+        Assert.assertEquals(connectionType, info.connectionType())
+    }
+
+    /**
+     * Check that a valid IP address can be obtained if a wifi or mobile network
+     * is enabled and a connection to the network is present. If no data transfer
+     * connection is found or both networks are disabled then a null String is
+     * given and the test is ignored. The localhost IP is not considered acceptable.
+     */
+    @Test
+    fun validIPConnection() {
+        val info = DeviceNetInfo(context)
+        Assert.assertNotNull(info.ipAddress())
+        Log.d("RelayzIO_android_net", "IP is ${info.ipAddress()}")
     }
 }
--- net/src/main/java/com/relayzio/android/net/DeviceNetInfo.kt
+++ net/src/main/java/com/relayzio/android/net/DeviceNetInfo.kt
@@ -1,7 +1,67 @@
 package com.relayzio.android.net
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import java.net.InetAddress
+import java.net.Inet4Address
+import java.net.Inet6Address
+import java.net.NetworkInterface
+import java.util.*
 
-class DeviceNetInfo (appContext : Context) {
+class DeviceNetInfo(appContext: Context) {
+    // Current connection type
+    private var connType: Int? = null
+    // Current wifi/mobile IP address
+    private var ipv4: String? = null
+    private var ipv6: String? = null
 
+    private val connManager = appContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    init {
+        queryNetworkInfo()
+    }
+
+    /**
+     * Returns the type of network enabled on the device or Null if no network is found.
+     * The type will be ConnectivityManager.TYPE_MOBILE or ConnectivityManager.TYPE_WIFI.
+     */
+    fun connectionType(): Int? {
+        return connType
+    }
+
+    /**
+     * Returns a valid IP for network connections through wifi or mobile. Preference is given
+     * for IPv4 over IPv6, if both exist.
+     */
+    fun ipAddress(): String? {
+        if (ipv4 != null)
+            return ipv4
+        return ipv6
+    }
+
+    /**
+     * Queries for and retrieves current network information on the device.
+     */
+    private fun queryNetworkInfo() {
+        // Get connection type
+        val activeNetwork: NetworkInfo? = connManager.activeNetworkInfo
+        connType = activeNetwork?.type
+
+        // Get IP if a network connection has been established
+        if (activeNetwork?.isConnected == true) {
+            val networks: Enumeration<NetworkInterface> = NetworkInterface.getNetworkInterfaces()
+            for (network in networks) {
+                val addresses: Enumeration<InetAddress> = network.inetAddresses
+                for (address in addresses) {
+                    if (!address.isLoopbackAddress) {
+                        if (address is Inet4Address)
+                            ipv4 = address.hostAddress
+                        else if (address is Inet6Address)
+                            ipv6 = address.hostAddress
+                    }
+                }
+            }
+        }
+    }
 }
